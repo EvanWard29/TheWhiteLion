81372d63d9ecde3f075da81d4148bba5a1a93a90
Evan Ward <evan.ward@students.plymouth.ac.uk>
2019-12-10 17:49:22 +0000

    Completed Design of Alcoholic Drinks
    Need to program buttons and connect to database

--- PubApplication/public/alcoholicDrinks.php
+++ PubApplication/public/alcoholicDrinks.php
@@ -4,20 +4,75 @@
 <div class="w3-content" style="max-width:1100px">
     <div class="w3-row w3-padding-64" id="menu">
         <div class="w3-col 20 w3-padding-large">
-            <h1 class="w3-center" style="font-size: 40px"><u>Drinks</u></h1><br>
+            <h1 class="w3-center" style="font-size: 40px"><u>Alcoholic Drinks</u></h1><br>
 
-            <button class="collapsible">Alcoholic Drinks<img src="../assets/img/secam_logo.jpg" style="padding-left: 100px"></button>
-            <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/cider.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Draught & Bottled Cider</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/cider.html' ?>
+                </div>
+
             </div>
+
             <hr>
-            <button class="collapsible">Soft Drinks</button>
-            <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/beer.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Beer</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/beer.html' ?>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/gin.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Gin</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/gin.html' ?>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/vodka.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Vodka</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/vodka.html' ?>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/rum.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Rum</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/rum.html' ?>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/whiskey.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Whiskey</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/whiskey.html' ?>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/wine.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Wine</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/wine.html' ?>
+                </div>
+            </div>
+
+            <div class="w3-container">
+                <button class="collapsible"><img src="../assets/img/champagne.jpg" style="width: 183.75px; height: 120px; border: 5px solid black;">&nbsp;&nbsp; Champagne</button>
+                <div class="content">
+                    <?php include 'alcoholicDrinks/champagne.html' ?>
+                </div>
             </div>
 
         </div>
@@ -26,22 +81,7 @@
 
 <?php include_once 'footer.php'; ?>
 
-<script>
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
-</script>
+<script src="../assets/js/collapsibleSections.js"></script>
 
 </body>
 
